3500e35ff16550d3e1cd78105baad557b2fcb876
reuhl <thomas.reuhl@telekom.de>
2015-03-12 12:16:06 +0100

    Added berks example
    Added a simple example for using berks with chef recipes

---
+++ chef-examples/cookbooks/berkstutorial/metadata.rb
@@ -0,0 +1,9 @@
+name             'berkstutorial'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures berkstutorial'
+long_description 'Installs/Configures berkstutorial'
+version          '0.1.0'
+
+depends 'getting-started'
---
+++ chef-examples/cookbooks/berkstutorial/recipes/default.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook Name:: berkstutorial
+# Recipe:: default
+#
+# Copyright (C) 2015 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+include_recipe "getting-started::default" 
