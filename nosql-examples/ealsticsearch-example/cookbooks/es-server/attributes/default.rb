3a831804ca5d2791b683566b1f248908469c9483
“tom1299” <tom1299@jeeatwork.com>
2015-03-19 16:12:57 +0100

    Added missing java recipe attribite

--- nosql-examples/ealsticsearch-example/cookbooks/es-server/attributes/default.rb
+++ nosql-examples/ealsticsearch-example/cookbooks/es-server/attributes/default.rb
@@ -1,8 +1,9 @@
 # Details of the java version to install
-default['java']['install_flavor'] = 'openjdk'
-default['java']['jdk_version'] = 7
+default['java']['install_flavor'] = 'oracle'
+default['java']['jdk_version'] = 8
 default['java']['set_etc_environment'] = true
 default['java']['set_default'] = true
+default['java']['oracle']['accept_oracle_download_terms'] = true
 
 # Set proxies here if required
 default['es-server']['all_proxy'] = nil
