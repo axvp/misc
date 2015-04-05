86542855861eb840619646d34264586af7f17df6
root <root@dev.jeeatwork.com>
2015-04-05 14:47:29 +0200

    Added remote file for eclipse luna

--- dev-setup/attributes/default.rb
+++ dev-setup/attributes/default.rb
@@ -8,3 +8,6 @@ default['java']['oracle']['accept_oracle_download_terms'] = true
 # Details for the maven version to install
 default['maven']['version'] = 3
 default['maven']['setup_bin'] = true
+
+# Details of the eclipse version to install
+default['dev-setup']['eclipse-download-url']= 'https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/luna/SR2/eclipse-java-luna-SR2-linux-gtk-x86_64.tar.gz&r=1'
--- dev-setup/recipes/default.rb
+++ dev-setup/recipes/default.rb
@@ -6,6 +6,12 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
 include_recipe "java::default"
 include_recipe "java::default_java_symlink"
+
 include_recipe "maven::default"
+
+remote_file "#{Chef::Config[:file_cache_path]}/eclipse.tar.gz" do
+  source node['dev-setup']['eclipse-download-url']
+end
