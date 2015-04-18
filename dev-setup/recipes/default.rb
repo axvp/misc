62ddd05aae74ad7921efabc3b1fc83fb08af0983
tom1299 <tom1299@jeeatwork.com>
2015-04-18 16:15:29 +0200

    Added vim installation

--- dev-setup/recipes/default.rb
+++ dev-setup/recipes/default.rb
@@ -22,9 +22,31 @@ remote_file eclipse_archive do
   source node['dev-setup']['eclipse-download-url']
 end
 
-bash 'install eclipse' do
+bash "install eclipse" do
   code <<-EOH
     tar xzf #{eclipse_archive} -C #{eclipse_install_dir}
     EOH
   not_if { ::File.exists?("/opt/eclipse") }
 end
+
+#
+# Create link to eclipse. See https://docs.chef.io/resource_link.html#examples
+#
+link "/opt/eclipse/eclipse" do
+  to "/usr/bin/eclipse"
+  not_if { ::File.exists?("/opt/eclipse/eclipse") }
+end
+
+#
+# Install vim
+#
+package "vim" do
+  action :install
+end
+
+#
+# Set vim as default editor in /etc/bashrc
+# alias vi='vim'. See:
+# http://stackoverflow.com/questions/16879469/using-a-chef-recipe-to-append-multiple-lines-to-a-config-file
+# http://stackoverflow.com/questions/14848110/how-i-can-change-a-file-with-chef/19167106#19167106
+#
