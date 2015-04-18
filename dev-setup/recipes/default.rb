608c9420f8955e7b2b3ac9f877c6df1f5ebaab04
tom1299 <tom1299@jeeatwork.com>
2015-04-18 11:47:48 +0200

    Completed eclipse installation

--- dev-setup/recipes/default.rb
+++ dev-setup/recipes/default.rb
@@ -12,6 +12,19 @@ include_recipe "java::default_java_symlink"
 
 include_recipe "maven::default"
 
-remote_file "#{Chef::Config[:file_cache_path]}/eclipse.tar.gz" do
+#
+# Eclipse installation. See: https://docs.chef.io/resource_remote_file.html
+#
+eclipse_archive = "#{Chef::Config[:file_cache_path]}/eclipse.tar.gz"
+eclipse_install_dir = "/opt"
+
+remote_file eclipse_archive do
   source node['dev-setup']['eclipse-download-url']
+end
+
+bash 'install eclipse' do
+  code <<-EOH
+    tar xzf #{eclipse_archive} -C #{eclipse_install_dir}
+    EOH
+  not_if { ::File.exists?("/opt/eclipse") }
 end
