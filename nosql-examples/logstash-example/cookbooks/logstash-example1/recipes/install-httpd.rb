913742d245effdf88ddaef0d4a15345b402ab8e6
tom1299 <tom1299@jeeatwork.com>
2015-04-23 14:21:56 +0200

    Removed ssl installation

--- nosql-examples/logstash-example/cookbooks/logstash-example1/recipes/install-httpd.rb
+++ nosql-examples/logstash-example/cookbooks/logstash-example1/recipes/install-httpd.rb
@@ -2,17 +2,17 @@ httpd_service 'default' do
   action [:create]
 end 
 
-httpd_module 'ssl' do
-  action :create
-end
-
 #
 # Install mod jk
 #
+# Using wget because of proxy problems when using remote_file
+#
 modjk_rpm = "#{Chef::Config[:file_cache_path]}/" + node['mod_jk']['filename']
-remote_file modjk_rpm do
-    source node['mod_jk']['download_url']
-    action :create
+
+execute "Downloading #{node['mod_jk']['filename']}" do
+   cwd Chef::Config[:file_cache_path]
+   command "wget #{node['mod_jk']['download_url']}"
+   not_if { ::File.exists?("#{modjk_rpm}")}
 end
 
 rpm_package modjk_rpm do
