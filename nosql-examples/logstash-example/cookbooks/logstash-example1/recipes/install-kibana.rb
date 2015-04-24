9af74f80f1a3642782416adb3109a54d9ed9b567
tom1299 <tom1299@jeeatwork.com>
2015-04-24 14:07:13 +0200

    Fixed bug in kibana recipe

--- nosql-examples/logstash-example/cookbooks/logstash-example1/recipes/install-kibana.rb
+++ nosql-examples/logstash-example/cookbooks/logstash-example1/recipes/install-kibana.rb
@@ -1,5 +1,5 @@
 #
-# Cookbook Name:: logstash-server
+# Cookbook Name:: logstash-example1
 # Recipe:: default
 #
 # Copyright 2015, YOUR_COMPANY_NAME
@@ -7,23 +7,23 @@
 # All rights reserved - Do Not Redistribute
 #
 
-remote_file "#{Chef::Config[:file_cache_path]}/kibana-" + node['logstash-server']['kibana_version'] + "-linux-x64.tar.gz" do
-  source "https://download.elasticsearch.org/kibana/kibana/kibana-" + node['logstash-server']['kibana_version'] + "-linux-x64.tar.gz"
+remote_file "#{Chef::Config[:file_cache_path]}/kibana-" + node['logstash-example1']['kibana_version'] + "-linux-x64.tar.gz" do
+  source "https://download.elasticsearch.org/kibana/kibana/kibana-" + node['logstash-example1']['kibana_version'] + "-linux-x64.tar.gz"
 end
 
 # Extract tar
 # Better way: https://github.com/gofullstack/tar-cookbook
 execute "Unzipping kibnan" do
-  command "tar -C /opt -xzf " + "#{Chef::Config[:file_cache_path]}/kibana-" + node['logstash-server']['kibana_version'] + "-linux-x64.tar.gz"
+  command "tar -C /opt -xzf " + "#{Chef::Config[:file_cache_path]}/kibana-" + node['logstash-example1']['kibana_version'] + "-linux-x64.tar.gz"
 end
 
-directory "/opt/kibana-" + node['logstash-server']['kibana_version'] + "-linux-x64" do
+directory "/opt/kibana-" + node['logstash-example1']['kibana_version'] + "-linux-x64" do
   owner "root"
   group "root"
   recursive true
 end
 
-log "/opt/kibana-" + node['logstash-server']['kibana_version'] + "-linux-x64" do
+log "/opt/kibana-" + node['logstash-example1']['kibana_version'] + "-linux-x64" do
   level :info
 end
 
@@ -35,7 +35,7 @@ end
 # TODO: Start kibana as a service
 bash 'start kibana' do
   user 'root'
-  cwd "/opt/kibana-" + node['logstash-server']['kibana_version'] + "-linux-x64"
+  cwd "/opt/kibana-" + node['logstash-example1']['kibana_version'] + "-linux-x64"
   code <<-EOH
   /opt/kibana-4.0.2-linux-x64/bin/kibana > kibana.log 2>&1 &
   EOH
