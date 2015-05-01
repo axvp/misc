5f03f8f37c522f65b4f8cfd1a0777a9b1920182d
tom1299 <tom1299@jeeatwork.com>
2015-05-01 11:10:21 +0200

    Fixed typo in package name

--- jee-examples/simple-stateless/src/main/java/com/jeeatwork/jee/examples/simple/stateless/CalculatorBean.java
+++ jee-examples/simple-stateless/src/main/java/com/jeeatwork/jee/examples/simple/stateless/CalculatorBean.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.jeeatwor.jee.examples.simple.stateless;
+package com.jeeatwork.jee.examples.simple.stateless;
 
 import javax.ejb.Stateless;
 
--- jee-examples/simple-stateless/src/test/java/com/jeeatwork/jee/examples/simple/stateless/CalculatorTest.java
+++ jee-examples/simple-stateless/src/test/java/com/jeeatwork/jee/examples/simple/stateless/CalculatorTest.java
@@ -14,14 +14,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.jeeatwor.jee.examples.simple.stateless;
+package com.jeeatwork.jee.examples.simple.stateless;
 
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import com.jeeatwor.jee.examples.simple.stateless.CalculatorBean;
+import com.jeeatwork.jee.examples.simple.stateless.CalculatorBean;
 
 import javax.ejb.embeddable.EJBContainer;
 import javax.naming.NamingException;
