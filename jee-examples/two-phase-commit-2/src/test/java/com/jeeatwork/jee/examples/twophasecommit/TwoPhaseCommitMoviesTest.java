d8fdcaed6058301ec0b8931ee85102cb4bc45497
tom1299 <tom1299@jeeatwork.com>
2015-05-05 12:36:37 +0200

    Databases now being created

--- jee-examples/two-phase-commit-2/src/test/java/com/jeeatwork/jee/examples/twophasecommit/TwoPhaseCommitMoviesTest.java
+++ jee-examples/two-phase-commit-2/src/test/java/com/jeeatwork/jee/examples/twophasecommit/TwoPhaseCommitMoviesTest.java
@@ -4,32 +4,37 @@ import java.util.Properties;
 
 import javax.ejb.embeddable.EJBContainer;
 import javax.naming.Context;
-import javax.naming.NamingException;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class TwoPhaseCommitMoviesTest {
-	
+
 	private static Context TEST_CONTEXT;
 
 	@BeforeClass
-	public static void createDBs() throws NamingException {
-        Properties p = new Properties();
-        p.put("movieDatabase", "new://Resource?type=DataSource");
-        p.put("movieDatabase.JdbcDriver", "org.hsqldb.jdbcDriver");
-        p.put("movieDatabase.JdbcUrl", "jdbc:hsqldb:mem:moviedb");
-        
-        p.put("movieDatabase2", "new://Resource?type=DataSource");
-        p.put("movieDatabase2.JdbcDriver", "org.hsqldb.jdbcDriver");
-        p.put("movieDatabase2.JdbcUrl", "jdbc:hsqldb:mem:moviedb");
-		
+	public static void createDBs() throws Exception {
+		Properties p = new Properties();
+		p.put("movieDatabase", "new://Resource?type=DataSource");
+		p.put("movieDatabase.JdbcDriver", "org.hsqldb.jdbcDriver");
+		p.put("movieDatabase.JdbcUrl", "jdbc:hsqldb:mem:moviedb");
+
+		p.put("movieDatabase2", "new://Resource?type=DataSource");
+		p.put("movieDatabase2.JdbcDriver", "org.hsqldb.jdbcDriver");
+		p.put("movieDatabase2.JdbcUrl", "jdbc:hsqldb:mem:moviedb2");
+
 		TEST_CONTEXT = EJBContainer.createEJBContainer(p).getContext();
+		
+		TwoPhaseCommitMoviesDBCreator moviesDBCreator = (TwoPhaseCommitMoviesDBCreator) TEST_CONTEXT
+				.lookup("java:global/two-phase-commit-2/TwoPhaseCommitMoviesDBCreator");
+		moviesDBCreator.createDatabases();
 	}
-	
+
 	@Test
 	public void test1() throws Exception {
-		
+
+		TwoPhaseCommitMoviesFacade moviesFacade = (TwoPhaseCommitMoviesFacade) TEST_CONTEXT
+				.lookup("java:global/two-phase-commit-2/TwoPhaseCommitMoviesFacade");
 	}
 
 }
