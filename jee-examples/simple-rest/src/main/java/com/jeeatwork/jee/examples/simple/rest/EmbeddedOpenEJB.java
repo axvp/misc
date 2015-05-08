311cd0ca7999c3d0552b42c54e2cc7715c13b06e
tom1299 <tom1299@jeeatwork.com>
2015-05-08 08:54:05 +0200

    Added lookup of simple rest service to embedded openejb

--- jee-examples/simple-rest/src/main/java/com/jeeatwork/jee/examples/simple/rest/EmbeddedOpenEJB.java
+++ jee-examples/simple-rest/src/main/java/com/jeeatwork/jee/examples/simple/rest/EmbeddedOpenEJB.java
@@ -3,9 +3,12 @@
  */
 package com.jeeatwork.jee.examples.simple.rest;
 
+import static org.junit.Assert.assertNotNull;
+
 import java.util.Properties;
 
 import javax.ejb.embeddable.EJBContainer;
+import javax.naming.Context;
 
 /**
  * @author tom1299
@@ -23,17 +26,20 @@ public class EmbeddedOpenEJB {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-    	Properties p = new Properties();
+	public static void main(String[] args) throws Exception {
+		Properties p = new Properties();
 
-    	p.put("httpejbd", "cxf-rs");
-    	p.put("openejb.embedded.remotable", "true");
-    	
-    	p.put("log4j.category.OpenEJB.options", "debug");
-    	p.put("log4j.category.OpenEJB.startup", "debug");
-    	p.put("log4j.category.OpenEJB.startup.config", "debug");
-    	
-        EJBContainer.createEJBContainer(p).getContext();
+		p.put("httpejbd", "cxf-rs");
+		p.put("openejb.embedded.remotable", "true");
+
+		p.put("log4j.category.OpenEJB.options", "debug");
+		p.put("log4j.category.OpenEJB.startup", "debug");
+		p.put("log4j.category.OpenEJB.startup.config", "debug");
+
+		Context context = EJBContainer.createEJBContainer(p).getContext();
+
+		Object object = context.lookup("java:global/simple-rest/SimpleRESTEJB");
+		assertNotNull(object);
 	}
 
 }
