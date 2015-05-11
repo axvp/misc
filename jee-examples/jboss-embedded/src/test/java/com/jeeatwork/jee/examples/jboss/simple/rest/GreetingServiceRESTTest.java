2988d3987f8591a2c1071de061d1d6e64d76a4f8
tom1299 <tom1299@jeeatwork.com>
2015-05-11 16:30:06 +0200

    Added standalone test case for rest
    Includes adding dependency to pom

---
+++ jee-examples/jboss-embedded/src/test/java/com/jeeatwork/jee/examples/jboss/simple/rest/GreetingServiceRESTTest.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package com.jeeatwork.jee.examples.jboss.simple.rest;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+
+import javax.ws.rs.client.WebTarget;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.extension.rest.client.ArquillianResteasyResource;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author tom1299
+ *
+ */
+@RunWith(Arquillian.class)
+public class GreetingServiceRESTTest {
+
+	@Deployment(testable = false)
+	public static WebArchive createDeployment() {
+		WebArchive webapp = ShrinkWrap.create(WebArchive.class,
+				"jboss-embedded-web.war").addClass(GreetingService.class);
+		webapp.setWebXML(new File("src/test/resources/web.xml"));
+		return webapp;
+	}
+
+	@Test
+	public void banCustomerRaw(@ArquillianResteasyResource WebTarget webTarget) {
+		String message = webTarget.path("/greeting").request().get().readEntity(String.class);
+		assertEquals("Hi REST!", message);
+	}
+}
