c5ca86e9b16c69b6dca6b6ee55b8e091d8c217a5
tom1299 <tom1299@jeeatwork.com>
2015-06-17 08:26:37 +0200

    Added sources

---
+++ microservice-examples/spring-boot-swagger-example/src/test/java/com/jeeatwork/microservices/spring/boot/swagger/HelloworldApplicationTest.java
@@ -0,0 +1,31 @@
+package com.jeeatwork.microservices.spring.boot.swagger;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.client.RestTemplate;
+
+import com.jeeatwork.microservices.spring.boot.swagger.HelloworldApplication;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = HelloworldApplication.class)
+@WebIntegrationTest({ "server.port=9999", "management.port=0" })
+public class HelloworldApplicationTest {
+
+	private RestTemplate restTemplate = new TestRestTemplate();
+
+	@Test
+	public void test1() {
+		ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:9999/helloworld", String.class);
+		assertEquals(response.getStatusCode(), HttpStatus.OK);
+		assertEquals("Hello World!", response.getBody());
+	}
+
+}
